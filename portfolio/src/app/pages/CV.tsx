53110da4f14dca4e97477a63335ffc823b60112b
Kylian <[kyliangachet@gmail.com]>
2024-12-26 11:30:29 +0100

    cleaning

--- portfolio/src/app/pages/CV.tsx
+++ portfolio/src/app/pages/CV.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import resume from '../assets/Resume.pdf';
 
 
 const CV: React.FC = () => {
@@ -10,8 +9,8 @@ const CV: React.FC = () => {
         <div className="w-40 h-1 bg-blue-500 mx-auto mb-20 rounded-full"></div>
         <p className="mb-6">Download my CV to find out more about my background and skills.</p>
         <a 
-          href={resume}
-          download="Resume"
+          href="${process.env.PUBLIC_URL}/CV.pdf"
+          download="CV.pdf"
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out inline-flex items-center"
         >
           <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
